5155bccbab32f0e664a9e9b2743b9961f79e57bc
don-ar <artem.donets.python@gmail.com>
2022-11-01 14:47:12 +0200

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -4,7 +4,7 @@ setup(
     name='clean_folder',
     version='1',
     description='To order your Forder',
-    url='http://github.com/dummy_user/useful',
+    url='https://github.com/kpi-donar/task7_goit.git',
     author='DAM',
     author_email='dam@example.com',
     license='MIT',
